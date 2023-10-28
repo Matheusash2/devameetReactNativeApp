8657564af3371999e7c5d9c6d2f7080137128780
Matheusash2 <127854649+Matheusash2@users.noreply.github.com>
2023-10-28 16:35:44 -0300

    Commit inicial
    Projeto do devameet em react native - devaria

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "../view/Login";
+
+const Routes = () => {
+    const Stack = createNativeStackNavigator();  
+    return (
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Login" component={Login}/>
+        </Stack.Navigator>
+    )
+}
+
+export default Routes;
---
+++ src/view/Login/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Login = () => {
+    return (
+        <View>
+            <Text>Teste de tela de login</Text>
+        </View>
+    )
+}
+
+export default Login;
